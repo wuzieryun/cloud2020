e60f10d447b0a7344d0c1089aafee11094c08b3b
wuzieryun <46878728+wuzieryun@users.noreply.github.com>
2021-05-31 21:29:35 +0800

    再加一个

--- cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 //加个注释
+//再加一个
 /**
  * @auther zzyy
  * @create 2020-02-18 21:15

d68fac234f169499f6d0ae7bd279e46ebf2cbdbf
wuziyun <wuziyun@qq.com>
2021-06-02 11:46:52 +0800

    531

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/service/controller/sendMessageController.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.service.controller;
+
+import com.atguigu.springcloud.service.IMessageProvider;
+import com.atguigu.springcloud.service.impl.MessageProviderImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class sendMessageController {
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage()
+    {
+        return messageProvider.send();
+    }
+
+}

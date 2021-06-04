e22e6996c3e77d4666bae2370d2edf87673b0e3a
wuziyun <wuziyun@qq.com>
2021-06-04 14:53:27 +0800

    531

--- cloudalibaba-provider-payment9001/src/main/java/controller/PaymentController.java
+++ cloudalibaba-provider-payment9001/src/main/java/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;

d9705ffa33839dace3192a9cd737b34574749f94
wuziyun <wuziyun@qq.com>
2021-06-08 17:24:24 +0800

    531

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auther zzyy
+ * @create 2020-01-09 16:34
+ */
+@RestController
+public class FlowLimitController
+{
+
+    @GetMapping("/testA")
+    public String testA()
+    {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB()
+    {
+        return "------testB";
+    }
+}
+
